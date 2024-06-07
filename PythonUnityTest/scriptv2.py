eb4a4c918d2787bb9fd8be9524244a8da421f872
Joshua Jones <88046193+JS-Jones@users.noreply.github.com>
2024-06-07 16:21:16 -0400

    Update scriptv2.py

--- PythonUnityTest/scriptv2.py
+++ PythonUnityTest/scriptv2.py
@@ -1,23 +1,40 @@
+import BertecRemoteControl
 import sys
+from time import sleep
 
-def main(args):
-    if len(args) < 1:
-        print("No command provided")
-        return
+global remote
+remote = BertecRemoteControl.RemoteControl()
+global res
+res = remote.start_connection()
 
-    command = args[0]
+def main(args, res):
 
-    if command == "print_string":
-        if len(args) < 2:
-            print("Python: No string provided to print")
-        else:
-            print("Python: The string is:", args[1])
-    elif command == "increase":
-        print("Python: Increase")
-    elif command == "decrease":
-        print("Python: Decrease")
-    else:
-        print("Python: Unknown command:", command)
+    while True:
+        if remote.is_client_authenticated()['result'] == "True":
+    
+            if (res is not None and res['code'] == 1):
+                command = args[0] if len(args) > 0 else None
+
+                if command == "Move":
+                    if len(args) < 2:
+                        print("No value provided for Move command")
+                    else:
+                        value = float(args[1])
+
+                        res = remote.run_treadmill(value, 1, 1, value, 1, 1)
+                        print(f"Move command executed with value {value}, response: {res}")
+                        
+                elif command == "Stop":
+                    res = remote.run_treadmill(0, 1, 1, 0, 1, 1)
+                    print("Stop command executed, response:", res)
+                    
+                else:
+                    print(f"Unknown command: {command}")
+                    
+
+                remote.stop_connection()
+                break
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    #main(sys.argv[1:])
+    main(sys.argv[1:], res)
